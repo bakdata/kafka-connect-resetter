99396efa68748801205912917fa9e7a16537a703
Philipp Schirmer <philipp.schirmer@bakdata.com>
2024-02-13 09:17:24 +0100

    Update dependencies (#15)
    Co-authored-by: Yannick Röder <yannick.roeder@bakdata.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,13 @@
-import net.researchgate.release.GitAdapter.GitConfig
-import net.researchgate.release.ReleaseExtension
-
 description = "An application to reset the state of Kafka Connect connectors"
 
 plugins {
     `java-library`
-    id("net.researchgate.release") version "2.8.1"
+    id("net.researchgate.release") version "3.0.2"
     id("com.bakdata.sonar") version "1.1.11"
     id("com.bakdata.sonatype") version "1.1.11"
-    id("org.hildan.github.changelog") version "0.8.0"
-    id("com.google.cloud.tools.jib") version "3.3.0"
-    id("io.freefair.lombok") version "5.3.3.3"
+    id("org.hildan.github.changelog") version "1.12.1"
+    id("com.google.cloud.tools.jib") version "3.4.0"
+    id("io.freefair.lombok") version "6.6.3"
 }
 
 allprojects {
@@ -51,27 +48,19 @@ configure<org.hildan.github.changelog.plugin.GitHubChangelogExtension> {
 dependencies {
     val kafkaVersion: String by project
     implementation(group = "org.apache.kafka", name = "connect-json", version = kafkaVersion)
-    implementation(group = "info.picocli", name = "picocli", version = "4.6.1")
+    implementation(group = "info.picocli", name = "picocli", version = "4.7.5")
     implementation(group = "org.apache.kafka", name = "kafka-clients", version = kafkaVersion)
-    implementation(group = "com.bakdata.seq2", name = "seq2", version = "1.0.4")
-    val log4jVersion = "2.17.2"
-    implementation(group = "org.apache.logging.log4j", name = "log4j-slf4j-impl", version = log4jVersion)
+    implementation(group = "com.bakdata.seq2", name = "seq2", version = "1.0.11")
+    val log4jVersion = "2.22.1"
+    implementation(group = "org.apache.logging.log4j", name = "log4j-slf4j2-impl", version = log4jVersion)
 
-    val junitVersion = "5.9.1"
+    val junitVersion = "5.10.1"
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-params", version = junitVersion)
     testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
-    testImplementation(group = "org.assertj", name = "assertj-core", version = "3.23.1")
-    testImplementation(group = "net.mguenther.kafka", name = "kafka-junit", version = kafkaVersion) {
+    testImplementation(group = "org.assertj", name = "assertj-core", version = "3.25.1")
+    testImplementation(group = "net.mguenther.kafka", name = "kafka-junit", version = "3.5.0") {
         exclude(group = "org.slf4j", module = "slf4j-log4j12")
     }
     testImplementation(group = "org.apache.kafka", name = "connect-file", version = kafkaVersion)
-}
-
-fun ReleaseExtension.git(configure: GitConfig.() -> Unit) = (getProperty("git") as GitConfig).configure()
-
-release {
-    git {
-        requireBranch = "main"
-    }
 }
